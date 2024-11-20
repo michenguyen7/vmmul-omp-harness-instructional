e3b28cd8691594f5112dcad7b1b977cde5f7fab9
Michelle <ellesfavorite@gmail.com>
2024-11-19 17:43:04 -0800

    fixed errors

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -7,11 +7,13 @@ const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double *A, double *x, double *y)
+// insert your code here: implementation of vectorized vector-matrix multiply
 {
-   // insert your code here: implementation of vectorized vector-matrix multiply
+   // lop over each row of the matrix A
    for (int i = 0; i < n; ++i)
    {
-      double temp = 0.0;
+      double temp = 0.0; // temp variable to store the result of the multiplication
+      // loop over each column of the matrix A
       for (int j = 0; j < n; ++j)
       {
          temp += A[i * n + j] * x[j];
